555f0d3a3bfc4a177596b5df6f2a868709bb0a01
Daksh04 <daksh04malhotra@gmail.com>
2026-01-30 23:26:22 +0530

    30012026

---
+++ 2977. Minimum Cost to Convert String II.py
@@ -0,0 +1,107 @@
+# 2977. Minimum Cost to Convert String II
+
+# [Hard]
+
+# You are given two 0-indexed strings source and target, both of length n and consisting of lowercase English characters. You are also given two 0-indexed string arrays original and changed, and an integer array cost, where cost[i] represents the cost of converting the string original[i] to the string changed[i].
+
+# You start with the string source. In one operation, you can pick a substring x from the string, and change it to y at a cost of z if there exists any index j such that cost[j] == z, original[j] == x, and changed[j] == y. You are allowed to do any number of operations, but any pair of operations must satisfy either of these two conditions:
+
+# The substrings picked in the operations are source[a..b] and source[c..d] with either b < c or d < a. In other words, the indices picked in both operations are disjoint.
+# The substrings picked in the operations are source[a..b] and source[c..d] with a == c and b == d. In other words, the indices picked in both operations are identical.
+# Return the minimum cost to convert the string source to the string target using any number of operations. If it is impossible to convert source to target, return -1.
+
+# Note that there may exist indices i, j such that original[j] == original[i] and changed[j] == changed[i].
+
+ 
+
+# Example 1:
+
+# Input: source = "abcd", target = "acbe", original = ["a","b","c","c","e","d"], changed = ["b","c","b","e","b","e"], cost = [2,5,5,1,2,20]
+# Output: 28
+# Explanation: To convert "abcd" to "acbe", do the following operations:
+# - Change substring source[1..1] from "b" to "c" at a cost of 5.
+# - Change substring source[2..2] from "c" to "e" at a cost of 1.
+# - Change substring source[2..2] from "e" to "b" at a cost of 2.
+# - Change substring source[3..3] from "d" to "e" at a cost of 20.
+# The total cost incurred is 5 + 1 + 2 + 20 = 28. 
+# It can be shown that this is the minimum possible cost.
+# Example 2:
+
+# Input: source = "abcdefgh", target = "acdeeghh", original = ["bcd","fgh","thh"], changed = ["cde","thh","ghh"], cost = [1,3,5]
+# Output: 9
+# Explanation: To convert "abcdefgh" to "acdeeghh", do the following operations:
+# - Change substring source[1..3] from "bcd" to "cde" at a cost of 1.
+# - Change substring source[5..7] from "fgh" to "thh" at a cost of 3. We can do this operation because indices [5,7] are disjoint with indices picked in the first operation.
+# - Change substring source[5..7] from "thh" to "ghh" at a cost of 5. We can do this operation because indices [5,7] are disjoint with indices picked in the first operation, and identical with indices picked in the second operation.
+# The total cost incurred is 1 + 3 + 5 = 9.
+# It can be shown that this is the minimum possible cost.
+# Example 3:
+
+# Input: source = "abcdefgh", target = "addddddd", original = ["bcd","defgh"], changed = ["ddd","ddddd"], cost = [100,1578]
+# Output: -1
+# Explanation: It is impossible to convert "abcdefgh" to "addddddd".
+# If you select substring source[1..3] as the first operation to change "abcdefgh" to "adddefgh", you cannot select substring source[3..7] as the second operation because it has a common index, 3, with the first operation.
+# If you select substring source[3..7] as the first operation to change "abcdefgh" to "abcddddd", you cannot select substring source[1..3] as the second operation because it has a common index, 3, with the first operation.
+ 
+
+# Constraints:
+
+# 1 <= source.length == target.length <= 1000
+# source, target consist only of lowercase English characters.
+# 1 <= cost.length == original.length == changed.length <= 100
+# 1 <= original[i].length == changed[i].length <= source.length
+# original[i], changed[i] consist only of lowercase English characters.
+# original[i] != changed[i]
+# 1 <= cost[i] <= 106
+
+# Code: Python 3
+
+class Solution:
+    def minimumCost(self, source, target, original, changed, cost):
+        INF = 10**30
+        id = {}
+        lens = set()
+        sz = 0
+
+        dist = [[INF]*201 for _ in range(201)]
+
+        for s, t, c in zip(original, changed, cost):
+            if s not in id:
+                id[s] = sz
+                lens.add(len(s))
+                sz += 1
+            if t not in id:
+                id[t] = sz
+                sz += 1
+            dist[id[s]][id[t]] = min(dist[id[s]][id[t]], c)
+
+        for i in range(sz):
+            dist[i][i] = 0
+
+        for k in range(sz):
+            for i in range(sz):
+                if dist[i][k] < INF:
+                    for j in range(sz):
+                        if dist[k][j] < INF:
+                            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+        n = len(source)
+        dp = [INF] * (n + 1)
+        dp[0] = 0
+
+        for i in range(n):
+            if dp[i] == INF:
+                continue
+
+            if source[i] == target[i]:
+                dp[i + 1] = min(dp[i + 1], dp[i])
+
+            for L in lens:
+                if i + L > n:
+                    continue
+                s = source[i:i+L]
+                t = target[i:i+L]
+                if s in id and t in id:
+                    dp[i + L] = min(dp[i + L], dp[i] + dist[id[s]][id[t]])
+
+        return -1 if dp[n] == INF else dp[n]
